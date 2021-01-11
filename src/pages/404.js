e8eb4c2c373711f52a0f6e696c962fe05daae481
Brandon-B91 <43705011+Brandon-B91@users.noreply.github.com>
2021-01-11 16:39:06 -0700

    minor changes to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Link from 'gatsby'
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -7,7 +7,6 @@ const NotFoundPage = () => (
   <Layout pageTitle="oops, something went wrong.">
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    <Link className="btn btn-secondary" to ={'/'}>Go Home</Link>
   </Layout>
 )
 
