4ac07bc1c63dab1c98e4b2a77d7cd734121fdf6b
Brandon-B91 <43705011+Brandon-B91@users.noreply.github.com>
2021-01-25 15:05:09 -0700

    changes to site title for issues with title name displaying incorreclty

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
     siteMetadata: {
-        title: `juniorDeveloper`,
+        title: `< juniorDeveloper />`,
         titleTemplate: `< juniorDeveloper />`,
         description: `Where juniorDevelopers come to talk about all things TechnoWizardry and MechanicalWitch.`,
         author: `Brandon Brown`,
