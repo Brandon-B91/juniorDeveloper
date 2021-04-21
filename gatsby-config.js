8993fbd6b122946246472abe1e0b79a67b8918f2
Brandon-B91 <43705011+Brandon-B91@users.noreply.github.com>
2021-04-21 16:31:54 -0600

    changes to seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -94,6 +94,7 @@ module.exports = {
       resolve: "gatsby-plugin-google-analytics",
       options: {
         trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || "none",
+        head: true
       },
     },
     "gatsby-plugin-preload-fonts",
