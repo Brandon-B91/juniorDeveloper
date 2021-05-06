4fc674ca80878a4280f5b89849df3c2b9d098464
Brandon-B91 <43705011+Brandon-B91@users.noreply.github.com>
2021-05-06 11:47:58 -0600

    removed google adsense plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -97,12 +97,6 @@ module.exports = {
         head: true
       },
     },
-    {
-      resolve: `gatsby-plugin-google-adsense`,
-      options: {
-        publisherId: `ca-pub-9588070657732142`
-      }
-    },
     "gatsby-plugin-preload-fonts",
     "gatsby-plugin-dark-mode",
   ],
