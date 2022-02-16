3c881faa68acd9abca0ef2492fda455e7f9137ae
Brandon-B91 <43705011+Brandon-B91@users.noreply.github.com>
2022-02-16 13:31:35 -0700

    removed offline support and pwainstall

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,10 +84,6 @@ module.exports = {
         icon: `src/images/code-icon.svg`, // This path is relative to the root of the site.
       },
     },
-
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
     {
       resolve: "gatsby-plugin-mailchimp",
       options: {
